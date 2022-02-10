d9fb6f712ae4832caf1ffdcbf98ab7bba92153a3
Rick McAlister <rick-mcalister@msn.com>
2022-02-09 15:24:32 -1000

    Modified the dome control code to deal with some underlying TSX race conditions

--- DomeControl.cs
+++ DomeControl.cs
@@ -74,34 +74,6 @@ namespace VariScan
             }
         }
 
-        public static void ReliableDomeHome()
-        {
-            //Method for homing my dome that ensures that wipers for power to the dome
-            //  are lined up correctly at the home position so power can flow!
-            // use exception handlers to check for dome commands, opt out if none
-            //  couple the dome to telescope if everything works out
-            //Method for opening the TSX dome 
-            sky6Dome tsxd = new sky6Dome();
-            //Decouple the dome from the telescope
-            IsDomeCoupled = false;
-            //Move dome to 20 degrees short of home position
-            //  then come back to make sure the home switch is activated accurately
-            //Get the az of the current postion -- probably home
-            tsxd.GetAzEl();
-            double azHome = tsxd.dAz;
-            //Move to 20 degrees CCW of current position
-            tsxd.GotoAzEl(azHome - 20, 0);
-            System.Threading.Thread.Sleep(1000);
-            //Wait until complete (Sync mode doesn//t seem to work)
-            while (tsxd.IsGotoComplete == 0) { System.Threading.Thread.Sleep(1000); };
-            //Home the dome,wait for the command to propogate, then wait until the dome reports it is homed
-            System.Threading.Thread.Sleep(6000);
-            tsxd.FindHome();
-            System.Threading.Thread.Sleep(1000);
-            while (tsxd.IsFindHomeComplete == 0) { System.Threading.Thread.Sleep(1000); };
-            return;
-        }
-
         public static bool OpenDome()
         {
             //Method to open dome
@@ -118,17 +90,13 @@ namespace VariScan
             //Stop whatever the dome might have been doing, if any and wait a few seconds for it to clear
             try { tsxd.Abort(); }
             catch { }
-            System.Threading.Thread.Sleep(5000);  //Wait for abort command to clear TSX and ASCOM driver
-                                                  //TSX throws error for some reason with this code, so maybe bypass it and hope
-            tsxd.FindHome();
-            System.Threading.Thread.Sleep(1000);
-            while (tsxd.IsFindHomeComplete == 0) { System.Threading.Thread.Sleep(1000); };
-
+            //Assume that the dome was either homed to close or doesn't need to
             tsxd.OpenSlit();
             System.Threading.Thread.Sleep(1000);  //Wait for close command to clear TSX and ASCOM driver
             while (tsxd.IsOpenComplete == 0)
             { System.Threading.Thread.Sleep(1000); } //one second wait loop
             IsDomeCoupled = true;
+            System.Threading.Thread.Sleep(1000);
             return true;
         }
 
@@ -150,21 +118,24 @@ namespace VariScan
             try { tsxd.Abort(); }
             catch { return; }
             //Wait for a second for the command to clear
-            System.Threading.Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(1000);
             //Close up the dome:  Connect, Home (so power is to the dome), Close the slit
             if (tsxd.IsConnected == 1)
             {
+                System.Threading.Thread.Sleep(1000);
                 tsxd.FindHome();
                 System.Threading.Thread.Sleep(1000);
-                while (tsxd.IsFindHomeComplete == 0) { System.Threading.Thread.Sleep(5000); };
+                while (tsxd.IsFindHomeComplete == 0)
+                { System.Threading.Thread.Sleep(1000); };
                 //Close slit
                 //Standard false stop avoidance code
                 bool slitClosed = false;
                 try
                 {
+                    System.Threading.Thread.Sleep(1000);
                     tsxd.CloseSlit();
                     System.Threading.Thread.Sleep(1000);
-                    while (tsxd.IsCloseComplete == 0) { System.Threading.Thread.Sleep(5000); }
+                    while (tsxd.IsCloseComplete == 0) { System.Threading.Thread.Sleep(1000); }
                     //Report success  
                     slitClosed = true;
                 }
@@ -176,11 +147,12 @@ namespace VariScan
                 //Check to see if slit got closed, if not, then try one more time
                 if (!slitClosed)
                 {
+                    System.Threading.Thread.Sleep(1000);
                     tsxd.CloseSlit();
                     System.Threading.Thread.Sleep(10000);
                     try
                     {
-                        while (tsxd.IsCloseComplete == 0) { System.Threading.Thread.Sleep(5000); }
+                        while (tsxd.IsCloseComplete == 0) { System.Threading.Thread.Sleep(1000); }
                     }
                     catch { }
                 }
